b7c3feea449d2f6f367a2592967923497e9b4221
Josh Goodwin <j@joshuagoodwin.com>
2014-01-23 03:35:35 +0000

    capitalise 'The' in 'the Bowland Auditorium

--- events/static/yorkunion.js
+++ events/static/yorkunion.js
@@ -14,12 +14,14 @@ jQuery(document).ready(function($) {
     }
     else if ($('.p-location').length) {
         var coordinates = [$('.p-latitude').attr('value'),$('.p-longitude').attr('value')];
+        var placeName = $('.p-location').html();
+        placeName = placeName.charAt(0).toUpperCase() + placeName.slice(1);
         var map = L.map('map').setView(coordinates, 16);
         L.tileLayer('http://www.york.ac.uk/about/maps/campus/data/tiles/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | <a href="http://www.york.ac.uk/about/maps/campus/">University of York</a>',
             maxZoom: 18,
             minZoom:13
         }).addTo(map);
-        L.marker(coordinates).addTo(map).bindPopup($('.p-location').html()).openPopup();
+        L.marker(coordinates).addTo(map).bindPopup(placeName).openPopup();
     }
 });

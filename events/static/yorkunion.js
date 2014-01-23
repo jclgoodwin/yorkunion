b15668baf48762fbb559fbdc74fd55d87dde9418
Josh Goodwin <j@joshuagoodwin.com>
2014-01-23 21:18:15 +0000

    map bounds (could be bigger for less zoomed in zoom levels)

--- events/static/yorkunion.js
+++ events/static/yorkunion.js
@@ -14,13 +14,13 @@ jQuery(document).ready(function($) {
     }
     else if ($('.p-location').length) {
         var coordinates = [$('.p-latitude').attr('value'),$('.p-longitude').attr('value')];
-        var placeName = $('.p-location').html();
-        placeName = placeName.charAt(0).toUpperCase() + placeName.slice(1);
+        var placeName = $('.p-location').text();
         var map = L.map('map').setView(coordinates, 16);
         L.tileLayer('http://www.york.ac.uk/about/maps/campus/data/tiles/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | <a href="http://www.york.ac.uk/about/maps/campus/">University of York</a>',
             maxZoom: 18,
-            minZoom:13
+            minZoom: 13,
+            maxBounds: [[53.916472204284254, -1.175537109375], [54.00857580366484, -0.9942626953125]]
         }).addTo(map);
         L.marker(coordinates).addTo(map).bindPopup(placeName).openPopup();
     }

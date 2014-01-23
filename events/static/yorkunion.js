665ab814881ce5a265702e3b8c7c51cd7789b50b
Josh Goodwin <j@joshuagoodwin.com>
2014-01-23 23:25:37 +0000

    turns out maxBounds belongs to the map, not the layer

--- events/static/yorkunion.js
+++ events/static/yorkunion.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function($) {
+
     if ($('#tumescence').length) {
         $('#tumescence').bjqs({
             height       : 480,
@@ -15,13 +16,17 @@ jQuery(document).ready(function($) {
     else if ($('.p-location').length) {
         var coordinates = [$('.p-latitude').attr('value'),$('.p-longitude').attr('value')];
         var placeName = $('.p-location').text();
-        var map = L.map('map').setView(coordinates, 16);
-        L.tileLayer('http://www.york.ac.uk/about/maps/campus/data/tiles/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | <a href="http://www.york.ac.uk/about/maps/campus/">University of York</a>',
-            maxZoom: 18,
-            minZoom: 13,
-            maxBounds: L.latLngBounds( L.latLng(53.916, -1.175), L.latLng(54.008, -0.994) ),
-        }).addTo(map);
+        var map = L.map('map', {
+            maxBounds: [[53.916, -1.175], [54.008, -0.994]],
+            center: coordinates,
+            zoom: 16,
+            layers: [L.tileLayer('http://www.york.ac.uk/about/maps/campus/data/tiles/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | <a href="http://www.york.ac.uk/about/maps/campus/">University of York</a>',
+                maxZoom: 18,
+                minZoom: 13,
+            })]
+        });
         L.marker(coordinates).addTo(map).bindPopup(placeName).openPopup();
     }
+
 });

4bcd0cb8b868bab6090e4a37ae95ce89d100b80a
Josh Goodwin <j@joshuagoodwin.com>
2014-01-23 01:08:56 +0000

    remove stray ] from yorkunion.js

--- events/static/yorkunion.js
+++ events/static/yorkunion.js
@@ -13,7 +13,7 @@ jQuery(document).ready(function($) {
         });
     }
     else if ($('.p-location')) {
-        var coordinates = [[$('.p-latitude').attr('value'),$('.p-longitude').attr('value')];
+        var coordinates = [$('.p-latitude').attr('value'),$('.p-longitude').attr('value')];
         var map = L.map('map').setView(coordinates, 16);
         L.tileLayer('http://www.york.ac.uk/about/maps/campus/data/tiles/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | <a href="http://www.york.ac.uk/about/maps/campus/">University of York</a>',

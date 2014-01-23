3a0932be0eb790e41bc22eb0c0f53db2c4446b72
Josh Goodwin <j@joshuagoodwin.com>
2014-01-23 02:42:58 +0000

    event image field

--- events/models.py
+++ events/models.py
@@ -30,6 +30,7 @@ class Event(models.Model):
     name = models.CharField(max_length=300)
     slug = models.SlugField(db_index=False)
     when = models.DateTimeField()
+    image = models.ImageField(null=True, blank=True)
     location = models.ForeignKey(Location, null=True, blank=True)
     facebook_event_id = models.BigIntegerField(null=True, blank=True)
     eventbrite_event_id = models.BigIntegerField(null=True, blank=True)

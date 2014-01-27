ea60443f6c59c2707983862e108fd9fe399e48a2
Josh Goodwin <j@joshuagoodwin.com>
2014-01-27 02:29:21 +0000

    add missing comma too

--- events/urls.py
+++ events/urls.py
@@ -11,7 +11,6 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^about/?$', views.about, name='about'),
     url(r'^press/?$', views.press, name='press'),
-    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': EventsSitemap})
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': EventsSitemap}),
     url(r'^(?P<slug>[-_\w]+)/?$', views.DetailView.as_view(), name='detail'),
-
 )

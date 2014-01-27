af6d5c54a9cf23f0b37af04e4e3d8a1deff3f513
Josh Goodwin <j@joshuagoodwin.com>
2014-01-27 02:28:36 +0000

    aargh fix tab/spaces mixture

--- events/urls.py
+++ events/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^about/?$', views.about, name='about'),
     url(r'^press/?$', views.press, name='press'),
-	url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': EventsSitemap})
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': EventsSitemap})
     url(r'^(?P<slug>[-_\w]+)/?$', views.DetailView.as_view(), name='detail'),
 
 )

53d16daa831708af146863e202b0cfc7a8b5f355
Josh Goodwin <j@joshuagoodwin.com>
2014-02-06 13:46:02 +0000

    favicon.ico

--- yorkunion/urls.py
+++ yorkunion/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
+    (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/favicon.ico'}),
     url(r'^', include('events.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )

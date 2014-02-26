3da18bcb3dcf40de6e868b1894e8d2459f8842e5
Josh Goodwin <j@joshuagoodwin.com>
2014-02-26 20:55:59 +0000

    more satisfactory urls (giving up on trailing slashlessness

--- events/urls.py
+++ events/urls.py
@@ -3,11 +3,6 @@ from django.shortcuts import redirect
 
 from events import views
 
-# urlpatterns = patterns('',
-    # url(r'^$', views.index, name='index')
-    # url(r'^', views.index, name='index')
-# )
-
 sitemaps = {
     'events': views.EventsSitemap,
     'static': views.StaticViewSitemap,
@@ -15,8 +10,8 @@ sitemaps = {
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^about/?$', views.about, name='about'),
-    url(r'^press/?$', views.press, name='press'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^press/', views.press, name='press'),
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
-    url(r'^(?P<slug>[-_\w]+)/?$', views.DetailView.as_view(), name='events'),
+    url(r'^(?P<slug>[-_\w]+)/$', views.DetailView.as_view(), name='events'),
 )

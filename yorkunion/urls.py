63fba7f3e90d4089569b67157dd5cee35e4ef84a
Josh Goodwin <j@joshuagoodwin.com>
2014-03-29 17:49:59 +0000

    remove grappelli

--- yorkunion/urls.py
+++ yorkunion/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^grappelli/', include('grappelli.urls')), # grappelli admin skin
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('events.urls')),
 )
